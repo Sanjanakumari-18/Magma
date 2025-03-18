3d541445d5c2306ca07676eb9e5b00242521c982
Sanjana Kumari <mehtasanjana7488@gmail.com>
2025-03-18 14:37:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const path=require('path');
 const express=require('express');
 const app=express();
+require('dotenv').config();
+
+const port = process.env.PORT || 4000;
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -10,5 +13,7 @@ app.get("/",function(req,res){
     res.render("index")
 })
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
